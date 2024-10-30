931c691042a5184d7db7a3bbc1066bf7a781b182
denistorjai <denistorjai@outlook.com>
2024-10-30 15:29:14 -0600

    Added and Modified for tests

--- src/implementations/Dictionary.java
+++ src/implementations/Dictionary.java
@@ -1,5 +1,6 @@
 package implementations;
 
+import exceptions.DuplicateKeyException;
 import utilities.DictionaryADT;
 import java.util.ArrayList;
 
@@ -9,8 +10,8 @@ public class Dictionary<K,V> implements DictionaryADT<K,V> {
     private static final int DEFAULT_SIZE = 10;
 
     // index indicates pairing, e.g. keys[1] is stored at values[1]
-    private ArrayList<K> keys;
-    private ArrayList<V> values;
+    private final ArrayList<K> keys;
+    private final ArrayList<V> values;
 
     // Hold Data
     public Dictionary() {
@@ -19,42 +20,57 @@ public class Dictionary<K,V> implements DictionaryADT<K,V> {
     }
 
     @Override
+    public void create(int size) {
+
+    }
+
+    @Override
     // Insert Key, First check if Key Already exists, if exists then don't add to Dictionary
-    public void insert(K key, V value) {
+    public boolean insert(K key, V value) throws DuplicateKeyException {
         if (this.keys.contains(key)) {
-            System.out.println("Key " + key + " already exists");
+            throw new DuplicateKeyException("Key already exists in the dictionary");
         } else {
             this.keys.add(key);
             this.values.add(value);
+            return true;
         }
     }
 
     @Override
     // Remove Key, Check if Key exists, then remove if they do
-    public void remove(K key) {
+    public V remove(K key) {
         if (this.keys.contains(key)) {
             int index = this.keys.indexOf(key);
             this.keys.remove(index);
+            V value = this.values.get(index);
             this.values.remove(index);
+            return value;
         }
+        return null;
     }
 
     @Override
     // Update Key, check if Key exists, get index of key, and update value based off of index
-    public void update(K key, V value) {
+    public boolean update(K key, V value) {
         if (this.keys.contains(key)) {
             int index = this.keys.indexOf(key);
             this.values.set(index, value);
+            return true;
         }
+        return false;
     }
 
     @Override
-    // Lookup
-    public V Lookup(K key) {
+    public V lookup(K key) {
         if (this.keys.contains(key)) {
             int index = this.keys.indexOf(key);
             return this.values.get(index);
         }
+        return null;
+    }
+
+    @Override
+    public V Lookup(K key) {
         return null;
     }
 
